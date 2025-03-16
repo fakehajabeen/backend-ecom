d854f43366541cf5fde6f996915a118873b8e340
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-03-16 20:10:49 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-const userSignUpController = require("../controller/user/userSignUp")
-const userSignInController = require('../controller/user/userSignIn')
+const userSignUpController = require("../controller/user/userSignup")
+const userSignInController = require('../controller/user/userSignin')
 const userDetailsController = require('../controller/user/userDetails')
 const authToken = require('../middleware/authToken')
 const userLogout = require('../controller/user/userLogout')
